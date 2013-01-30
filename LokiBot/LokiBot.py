20e0422dc8f57f00e110c9afbaf2a570de0097a5
NGat <nathan.gatenby@gmail.com>
2013-01-30 22:43:05 +1000

    Various changes
    Added more message functionality, linked with Jevist's 'BitBot', and
    other things. I'll probably tidy it up later on, it's quite messy as it
    is

--- LokiBot/LokiBot.py
+++ LokiBot/LokiBot.py
@@ -7,7 +7,6 @@ import sys
 import socket
 import string
 import os
-import pickle
 import re
 HOST = 'irc.esper.net'
 PORT = 6665
@@ -19,6 +18,7 @@ CHAN = '#tornkingdom'
 PASS = ''
 people = []
 peoplestr = ""
+names = {}
 s = socket.socket()
 ####
 
@@ -34,8 +34,8 @@ s.send('USER '+IDENT+' '+HOST+' bla :'+REALNAME+'\r\n')
 while 1:
   line=s.recv(500) #Getting data
   if line.startswith("PING"):
-    s.send("PONG " + line.split()[1]+"\r\n")
-    print "PONG " + line.split()[1]
+      s.send("PONG " + line.split()[1]+"\r\n")
+      "PONG " + line.split()[1]
   if line.find('already in use')!=-1:
         NICK = NICK + "_"
         s.send('USER '+IDENT+' '+HOST+' bla :'+REALNAME+'\r\n')
@@ -50,10 +50,16 @@ while 1:
 #### GETTING DATA AND PRELIMINARY ACTIONS ####
 while True:
     ### MANAGING CHAT ###
-  
-    line = s.recv(500) #Getting data
-    print line
+
+    line = s.recv(500)  #                         #Getting data
+    ingame = False
+
+    if line.startswith("PING"): #Esper PingPong
+        s.send("PONG " + line.split()[1]+"\r\n")
+        print "PONG " + line.split()[1]+"\n"
+
     man = line[1:(line.find("!"))] #Person who sent the line
+    sender=man
     splitall = line.split()
     split = line.split(CHAN) #Splitting the IP etc. from the message
     if line.find("Please choose a different nickname, or identify via /msg NickServ identify <password>")!=-1:
@@ -66,11 +72,14 @@ while True:
 
 #### PARSING ####
     if CHAN in line:
-
+        if line.index(CHAN)!=0:
+          mode = splitall[splitall.index(CHAN)-1]
         msg = split[1][2:]#gets the message
         match = re.search(r"...\[...G...\] ...\[...(.*?)...\] ...(.*?)......:... (.*)", msg) #Checks to see if the message is from Minecraft
         if match:#checks if the message is from MC
           msg = match.group(3)
+          sender = "TornKing"
+          ingame = True
           man = match.group(2)
 
           #### Working with Name exceptions (Admins) ####
@@ -78,57 +87,115 @@ while True:
             man = "Jevist"
           if man.find("Geachinator")!=-1:
             man = "Geachinator"
+          if man.find("Bob_Paul")!=-1:
+            man = "Bob_Paul"
           ####
             
-          print "TK " + man + ": " + msg
+          print "TK " + man + ": " + msg,
+        elif re.search(r"\[(.*?) .?.?.?connected\]", msg):
+          print "TK " + msg,
         else:
-          print CHAN, man, ":", msg
+          print CHAN, man + ":", msg,
         searcher = msg.rstrip().lower() #What to use when searching in a msg
         msgsplit = searcher.split()
 
 
         #### PINGPONG ####
         if searcher == "ping": #PINGPONG
-            s.send("PRIVMSG " + CHAN + " : PONG.\r\n")
+            s.send("PRIVMSG " + CHAN + " : pong\r\n")
         ####
-      
+
+        #### help message ####
+        REhelp = re.search(r"^(help)|halp", searcher)
+        if REhelp:
+          s.send("PRIVMSG " + CHAN + " : %s, if you need help from staff, write them a Petition! /pe new <message>\r\n" %man)
+
+
+        RETown = re.search(r'(how to start a town)|(how do i start a town)', msg.lower())
+        if RETown:
+          s.send("PRIVMSG " + CHAN + " : %s, you need to be Adventurer rank to start a town, that costs $1000. A town will cost you $3000. /t new <name>\r\n" %man)
 
         #### .users (getting who is on the IRC channel) ####
         if searcher == ".users":       #To let MC players see who is on IRC
           s.send("NAMES " + CHAN + '\r\n')
           line=s.recv(500)
-          splitall = line.split()
-          print "sent names"   # .users 
+          print line
+          splitall = line.split() 
           peoplestr = ""
           people = splitall[splitall.index(CHAN)+1:]
           people[0] = people[0][1:]
           for count in people:
             if count[0] == "@" or count[0] == "+":
               count = count[1:]
-            if count == "TornKing": #Getting rid of the bots off the list
+            if count.find("TornKing")!=-1:
               continue
-            if count == "Minebot":
+            if count.find("Minebot")!=-1:
               continue
             peoplestr+=count + " "
           s.send('PRIVMSG ' + CHAN + " : On #tornkingdom: " + peoplestr + '\r\n')
         ####
           
 
+
         #### Welcomer ####
-        con = re.search(r"\[(.*?) connected\]", msg)
-        if con:
+        con = re.search(r"\[(.*?) connected\]", msg)  
+        if mode == "JOIN":
+          rename = re.search(r':(.*?)!', line)
+          name = rename.group(1)
+          for nam in open("names.txt", "rU"):
+            names[nam.split()[0]] = nam.split(" ", 1)[1]
+          if name in names:
+            name = names[name]
+          s.send("PRIVMSG " + CHAN + " : Hey, %s\r\n" %name)
+          
+        if con and sender == "TornKing":
           name = con.group(1)
+          for nam in open("names.txt", "rU"):
+            names[nam.split()[0]] = nam.split(" ", 1)[1]
+          if name in names:
+            name = names[name]
           s.send("PRIVMSG " + CHAN + " : " + "Hey %s\r\n" %name)
         ####
 
-          
-        #### 'Kingdom Server finder ####
-        if searcher.find("kingdom server")!=-1 or searcher.find("kingdoms server")!=-1:
-          s.send("PRIVMSG " + CHAN + " : " + "We are a Towny Server, not a Kingdom Server! See http://tornkingdom.net for more info.\r\n")
-        ####
+        TRE = re.search(r'^.tr (.*)', msg)
+        if TRE and ingame:
+          print "success"
+          s.send("PRIVMSG BitBot :" + msg + "\r\n")
+          print "PRIVMSG BitBot : " + msg
 
-    #### Esper net's PING PONG ####
-    if line.startswith("PING"):
-        s.send("PONG " + line.split()[1]+"\r\n")
-        print "PONG " + line.split()[1]
+
+        #### 'Kingdom Server finder ####
+        kingdom = re.search(r'kingdom(s)? server|join a kingdom|join ur kingdom|join your kingdom', searcher)
+        if kingdom:
+          s.send("PRIVMSG " + CHAN + " : " + "%s, we are a Towny Server, not a Kingdom Server! See http://tornkingdom.net for more info.\r\n" %man)
+        ####
+        if searcher == 'debug':
+          print people
+          print man
+          print mode
+          print names
+          print ""
+          print line
+
+
+###End of 'if CHAN in line'###
+
+
+
+
+    #### PM stuff: rules, tips, LokiChat ####
+    pm = re.search(r'^:(.*?)!.*? PRIVMSG %s :(.*)' %NICK, line)
+    if pm:
+      user = pm.group(1)
+      pmg = pm.group(2)
+      if user == "Loki456789":
+        s.send("PRIVMSG " + CHAN + " : %s\r\n" %pmg)
+      elif pmg.find("rules")!=-1:
+        s.send("PRIVMSG "+user+" : 1. Be respectful to all players and users. No abuse or bullying will be tolerated.\r\nPRIVMSG "+user+" : 2. Do not spam chat or use offensive or harsh language.\r\nPRIVMSG "+user+" : 3. Do not impersonate players, users, or members of the greater public.\r\nPRIVMSG "+user+" : 4. Do not advertise other games or servers.\r\nPRIVMSG "+user+" : Failure to adhere to these rules may result in InGame and IRC Bans.\r\n")
+        print "PRIVMSG "+user+" : 1. blah"
+      elif pmg.find("tips")!=-1:
+         s.send("PRIVMSG "+user+" : Hi, welcome to " + CHAN + " IRC. Enjoy your stay!\r\nPRIVMSG "+user+" : Here are a few tips:\r\nPRIVMSG "+user+" : 1. When InGame, type .users to view a list of online IRC users.\r\nPRIVMSG "+user+" : 2. When on IRC, type .players to view a list of online players.\r\nPRIVMSG "+user+" : 3. Message me 'rules' to see a list of rules.\r\nPRIVMSG "+user+" : 4. Speak to Loki456789/LowQuai about changing your 'Login Nickname'\r\nPRIVMSG "+user+" : Enjoy your stay!\r\nPRIVMSG "+user+" : --LokiBot/Loki456789, and the IRC Staff.\r\n")
+      elif user == "BitBot":
+        s.send("PRIVMSG " + CHAN + " :"+pmg[9:-24]+")\r\n")
+      print "Message from " + user + ": " + pmg + '\n'
     ####
